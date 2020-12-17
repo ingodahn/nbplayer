8b1bf87bc53f00a542fa459c5e7203dc1d1c748c
Ingo Dahn <ingodahn@gmail.com>
2020-12-17 15:11:58 +0100

    Added bootstrap support

--- js/nbrunner.js
+++ js/nbrunner.js
@@ -52,6 +52,7 @@ function saveHtml() {
   '</head>\n<body>\n<div id="main">'+
   $('#main').html()+
   `</div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://sagecell.sagemath.org/embedded_sagecell.js"></script>
   <script src="./vendor/js/FileSaver.min.js"></script>
